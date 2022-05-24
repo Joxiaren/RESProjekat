e1201ef83791e441a39e632e40e9fc50b108a11d
unknown <nikolatankela@gmail.com>
2022-05-24 13:37:00 +0200

    Changed implementation for open/close connection functionality

--- WriterComponent.py
+++ WriterComponent.py
@@ -4,12 +4,10 @@ import rpyc
 def open_connection():
     #connecting to ReplicatorSenderService
     conn = rpyc.connect("localhost", 22277)
-    conn.on_connect()
     print("Writer connected.")
     return conn
 
 def close_connection(conn):
     #disconnecting from ReplicatorSenderService
-    conn.on_disconnect()
-    #command for disconnecting
+    del conn
     print("Writer disconnected.")
