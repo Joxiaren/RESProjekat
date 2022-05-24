07dccff1dfa8c3f3b58f234cba8e4235d64adebb
VidakGrujic <vidakspartanacgrujic@gmail.com>
2022-05-24 13:22:54 +0200

    Adding fuctionality that send data to Replicator Component

---
+++ WriterComponent.py
@@ -0,0 +1,35 @@
+import ReplicatorSender
+import rpyc
+
+
+def send_data(conn):
+    # here we should call function input_Data()
+    # idCounter, currentWaterConsuption = input_Data()
+    # dict =
+    #       {
+    #          "idCounter" : idCounter,
+    #          "currentWaterCounsuption" : currentWaterConsuption    
+    #       }
+    #
+    
+    conn.root.send_to_replicator(dict)
+
+
+if __name__ == "__main__": 
+
+
+    conn = open_connection()
+
+    while(True):
+        
+        send_data(conn)
+
+        print("For exit enter 'exit': ")
+        exit = input()
+        if(exit.lower == 'exit'):
+            close_connection(conn)
+            break    
+
+        
+
+
