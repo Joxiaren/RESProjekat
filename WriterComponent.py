2c9f19a78e32ce66d48fb4fbe18b9bd93ac2e2dc
filiptodorovic <filip.todorovic18@gmail.com>
2022-05-24 19:09:25 +0200

    Added the data input functionality

--- WriterComponent.py
+++ WriterComponent.py
@@ -1,5 +1,19 @@
 import rpyc
 
+def input_data():
+    print("Water meter ID: ")
+    idCounter=input()
+    if type(idCounter)!=int:
+        raise TypeError("Water meter ID must be an integer!")
+    print("Water consumption for that meter ID: ")
+    currentWaterCounsuption= input()
+    if type(currentWaterCounsuption)==int or type(currentWaterCounsuption)==float:
+        pass
+    else:
+        raise TypeError("The consumption is not a number!")
+
+    return idCounter,currentWaterCounsuption
+
 
 def open_connection():
     # connecting to ReplicatorSenderService
