549fc27b16fa3ac0a2a402366b783c3c2956d178
Tankela <nikolatankela@gmail.com>
2022-05-24 18:57:16 +0200

    Removed import ReplicatorSender

--- WriterComponent.py
+++ WriterComponent.py
@@ -1,13 +1,14 @@
-import ReplicatorSender
 import rpyc
 
+
 def open_connection():
-    #connecting to ReplicatorSenderService
+    # connecting to ReplicatorSenderService
     conn = rpyc.connect("localhost", 22277)
     print("Writer connected.")
     return conn
 
+
 def close_connection(conn):
-    #disconnecting from ReplicatorSenderService
+    # disconnecting from ReplicatorSenderService
     del conn
     print("Writer disconnected.")
