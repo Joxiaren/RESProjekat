13b57429f1f66fb0b2fa12a064e73847dd0fbd40
Tankela <nikolatankela@gmail.com>
2022-05-31 19:28:00 +0200

    Added test file for ReportsComponent

---
+++ testReportsPrint.py
@@ -0,0 +1,13 @@
+import unittest
+import ReportsComponent
+
+class TestPrintReportForSpecificStreet(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+class TestPrintReportForSpecificCounter(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+if __name__ == '__main__':
+    unittest.main()
