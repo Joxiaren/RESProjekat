0b69e35666aa20523749777e7b5d44940f8a169a
Tankela <nikolatankela@gmail.com>
2022-06-02 18:40:49 +0200

    Implemented tests for both function for formatted printing of reports

---
+++ ReportsComponent.py
@@ -0,0 +1,60 @@
+def getReportForSpecificStreet(street):
+    # should return Dictionary<string, float> with name of month as a key and water consumption in specific STREET as value
+    return
+
+
+def getReportForSpecificCounter(idCounter):
+    # should return Dictionary<string, float> with name of month as a key and water consumption on specific COUNTER as value
+    return
+
+
+
+def printReportForSpecificStreet(street, data):
+    # data is Dictionary<string, float> with name of month as a key and water consumption as value
+    print("Water consumption in street: " + street)
+    print("----------------------------------------------------")
+    for month in data:
+        print(month + " : " + str(data[month]))
+
+    print("----------------------------------------------------")
+    return
+
+
+def printReportForSpecificCounter(idCounter, data):
+    # data is Dictionary<string, float> with name of month as a key and water consumption as value
+    print("Water consumption on specific water meter with id: " + str(idCounter))
+    print("----------------------------------------------------")
+    for month in data:
+        print(month + " : " + str(data[month]))
+
+    print("----------------------------------------------------")
+    return
+
+
+
+class WrongNumberOfArguments(Exception):
+    def __init__(self, message=None):
+        self.message = message
+
+    def __str__(self):
+        if self.message is None:
+            return "You did not pass correct number of arguments in function call."
+        else:
+            return self.message
+
+
+
+def printFormatedReports(typeOfReport, street="", idCounter=-1):
+
+    if (typeOfReport == "street") and (street != ""):
+        dict = getReportForSpecificStreet(street)
+        printReportForSpecificStreet(street, dict)
+        return
+
+    elif (typeOfReport == "id") and (idCounter != -1):
+        dict = getReportForSpecificCounter(idCounter)
+        printReportForSpecificCounter(idCounter, dict)
+        return
+
+    else:
+        raise WrongNumberOfArguments()
