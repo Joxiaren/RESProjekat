be17e28c458aaf4d5149d5a5bb10a9949f8533d6
filiptodorovic <filip.todorovic18@gmail.com>
2022-06-09 20:51:06 +0200

    Initial connection concept

--- ReaderComponent.py
+++ ReaderComponent.py
@@ -1,3 +1,27 @@
+import rpyc
+from rpyc.utils.server import ThreadedServer
+
+
+class ReaderComponentService(rpyc.Service):
+    def on_connect(self, conn):
+        # code that runs when service is connected
+        print("i am connected")
+        pass
+
+    def on_disconnect(self, conn):
+        # code that runs when service is disconnected
+        print("i am disconnected")
+        pass
+
+    def exposed_print_message(self, message):
+        print(message)
+
+    def exposed_send_to_reader(self, data):
+        print("successfully received data")
+        print(data)
+        # data check
+        # send to temporary storage
+        # send_to_temp_storage(data)
 
 def connectToDatabase():
     pass
@@ -21,3 +45,7 @@ def writeToDatabase(data):
     except Exception as e:
         print(e)
 
+if __name__ == "__main__":
+    server = ThreadedServer(ReaderComponentService(), port=42277)
+    print("server started")
+    server.start()
