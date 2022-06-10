45d35b1cfe22892abb23315325f2011ef251aa23
Tankela <nikolatankela@gmail.com>
2022-06-10 21:27:33 +0200

    Implemented tests for send_data function in ReplicatorReciever

--- testSendDataToReader.py
+++ testSendDataToReader.py
@@ -1,15 +1,35 @@
 import unittest
-import ReplicatorReceiver
-from unittest.mock import patch
+from ReplicatorReceiver import ReplicatorReceiver as RR
+from unittest.mock import MagicMock
 
-@patch('ReaderService.send_to_reader')
+conn = MagicMock()
+conn.root = MagicMock()
+conn.root.send_to_reader = MagicMock()
+
+replicatorComponent = RR()
+
 class MyTestCase(unittest.TestCase):
-    def test_send(self, mocked_send_to_reader):
-        dictinary_list = [{"idMeter": 1, "consumption": 1.0, "month": "January"},
-                          {"idMeter": 2, "consumption": 2.0, "month": "February"},
-                          {"idMeter": 3, "consumption": 3.0, "month": "March"}]
-        ReplicatorReceiver.send_data(dictinary_list)
-        mocked_send_to_reader.assert_called_once_with(dictinary_list) # add assertion here
+    def test_send_regular_data(self):
+        data = [{"idMeter": 1, "consumption": 1.0, "month": "January"},
+                {"idMeter": 2, "consumption": 2.0, "month": "February"},
+                {"idMeter": 3, "consumption": 3.0, "month": "March"}]
+        replicatorComponent.send_data(conn,data)
+        conn.root.send_to_reader.assert_called_with(data)
+
+    def test_send_empty_data(self):
+        empty_data = []
+        replicatorComponent.send_data(conn, empty_data)
+        conn.root.send_to_reader.assert_called_with(empty_data)
+
+    def test_send_without_conn_parameter(self):
+        data = [{"idMeter": 1, "consumption": 1.0, "month": "January"},
+                {"idMeter": 2, "consumption": 2.0, "month": "February"},
+                {"idMeter": 3, "consumption": 3.0, "month": "March"}]
+        self.assertRaises(TypeError, replicatorComponent.send_data, data)
+
+    def test_send_without_data_parameter(self):
+        self.assertRaises(TypeError, replicatorComponent.send_data, conn)
+
 
 
 if __name__ == '__main__':
