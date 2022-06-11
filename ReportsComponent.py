46a6c9ad84b6c54024dd5f2d5b107a1e6062df80
Joxiaren <2000jox@gmail.com>
2022-06-11 14:28:10 +0200

    Added main functionality for client console interaction and added input verification method

--- ReportsComponent.py
+++ ReportsComponent.py
@@ -107,3 +107,52 @@ def printFormatedReports(typeOfReport, street="", idMeter=-1):
 
     else:
         raise WrongNumberOfArguments()
+
+
+def input_num(number, limit=-1):
+
+    user_input = int(number)
+    if number <= 0 or (limit != -1 and number > limit):
+        raise InputOutOfRange
+    return user_input
+
+
+def main():
+    while True:
+        print("Enter number in front of desired report")
+        print("1. Report for monthly consumption of a single street")
+        print("2. Report for consumption throughout months for one water meter")
+
+        while True:
+            try:
+                user_input = input_num(input(), 2)
+                break
+            except Exception as e:
+                print(e)
+
+        if user_input == 1:
+            print("Input street name")
+            # validation and return value should be double-checked (open to discussion)
+            street_name = input()
+            printReportForSpecificStreet(street_name, getReportForSpecificStreet(street_name, 'DataBase.db'))
+            pass
+        elif user_input == 2:
+            print("Input water meter id")
+            # validation and return value should be double-checked (open to discussion)
+            id_meter = input()
+            printReportForSpecificMeter(id_meter, getReportForSpecificMeter(id_meter, 'DataBase.db'))
+            pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+class InputOutOfRange(Exception):
+    def __init__(self, message=None):
+        self.message = message
+
+    def __str__(self):
+        if self.message is None:
+            return "Input number is out of option range"
+        return self.message
