fcb446840f0c98296c6b4d59c9f4263c376f2f85
Joxiaren <2000jox@gmail.com>
2022-06-11 12:44:09 +0200

    Fixed formatting

--- ReaderComponent.py
+++ ReaderComponent.py
@@ -1,28 +1,30 @@
 import sqlite3
 
+
 @staticmethod
-def connectToDatabase(dbName):
+def connect_to_database(dbName):
     connection_string = 'file:%s?mode=rw' % dbName
     conn = sqlite3.connect(dbName)
     return conn
 
 
 @staticmethod
-def disconnectFromDatabase(conn):
+def disconnect_from_database(conn):
     conn.close()
     return
 
-def writeToDatabase(data):
+
+def write_to_database(data):
     # data is list of dictionaries
     try:
-        conn = connectToDatabase('DataBase.db')
+        conn = connect_to_database('DataBase.db')
         cursor = conn.cursor()
 
         for dictionary in data:
             cursor.execute("INSERT INTO WATER_CONSUMPTION (idMeter, consumption, month) VALUES (:idMeter, :consumption, :month);",
                            {'idMeter': dictionary["idMeter"], 'consumption': dictionary["consumption"], 'month': dictionary["month"]})
             conn.commit()
-        disconnectFromDatabase(conn)
+        disconnect_from_database(conn)
     except Exception as e:
         print(e)
 
