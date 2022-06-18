822bbefe209ad7fe8c3958499660dfb69db61d2d
Tankela <nikolatankela@gmail.com>
2022-06-18 19:11:45 +0200

    Implemented tests for writing to Database

--- testReaderWriteToDatabase.py
+++ testReaderWriteToDatabase.py
@@ -1,27 +1,34 @@
 import unittest
 import ReaderComponent
 from unittest.mock import MagicMock
-
+from unittest.mock import patch
+from unittest.mock import call
 
 conn = MagicMock()
-conn.cursor = MagicMock()
-conn.cursor.execute = MagicMock()
-ReaderComponent.connectToDatabase = MagicMock()
 cursor = MagicMock()
-cursor.execute = MagicMock()
-conn.commit = MagicMock()
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
+conn.cursor.return_value = cursor
+
+class TestWriteToDatabase(unittest.TestCase):
+    @patch("ReaderComponent.connectToDatabase", return_value=conn)
+    def test_regular_list_of_dictionaries(self, connectPatch):
         data = [{"idMeter": 1, "consumption": 1.0, "month": "January"},
                 {"idMeter": 2, "consumption": 2.0, "month": "February"},
                 {"idMeter": 3, "consumption": 3.0, "month": "March"}]
+        calls = [call("INSERT INTO WATER_CONSUMPTION (idMeter, consumption, month) VALUES (:idMeter, :consumption, :month);",
+                           {'idMeter': data[0]["idMeter"], 'consumption': data[0]["consumption"], 'month': data[0]["month"]}),
+                 call("INSERT INTO WATER_CONSUMPTION (idMeter, consumption, month) VALUES (:idMeter, :consumption, :month);",
+                           {'idMeter': data[1]["idMeter"], 'consumption': data[1]["consumption"], 'month': data[1]["month"]}),
+                 call("INSERT INTO WATER_CONSUMPTION (idMeter, consumption, month) VALUES (:idMeter, :consumption, :month);",
+                           {'idMeter': data[2]["idMeter"], 'consumption': data[2]["consumption"], 'month': data[2]["month"]})]
         ReaderComponent.writeToDatabase(data)
-        #ReaderComponent.connectToDatabase.asser_called_once_with()
-        #conn.cursor.assert_called_with()
-        cursor.execute.assert_called_with("INSERT INTO WATER_CONSUMPTION (idMeter, consumption, month) VALUES (1, 1.0, January);")
-        #cursor.execute.assert_called_with("INSERT INTO WATER_CONSUMPTION (idMeter, consumption, month) VALUES (2, 2.0, February);")
-        #cursor.execute.assert_called_with("INSERT INTO WATER_CONSUMPTION (idMeter, consumption, month) VALUES (3, 3.0, March);")
-        #conn.commit.assert_called_with()
+
+        cursor.execute.assert_has_calls(calls)
+
+    @patch("ReaderComponent.connectToDatabase", return_value=conn)
+    def test_empty_list_of_dictionaries(self, connectPatch):
+        data = []
+        ReaderComponent.writeToDatabase(data)
+        cursor.execute.assert_not_called()
 
 
 if __name__ == '__main__':
