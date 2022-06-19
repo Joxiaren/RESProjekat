cf42a4d199a103f371977bf98af6753d8fe335eb
Joxiaren <2000jox@gmail.com>
2022-06-19 20:40:04 +0200

    Added tests and fixed small formatting mistakes

--- testReplicatorComponent.py
+++ testReplicatorComponent.py
@@ -1,9 +1,6 @@
-from asyncio import DatagramProtocol
 import unittest
 from unittest.mock import MagicMock, patch
-from unittest.mock import ANY
 import ReplicatorReceiver
-
 
 
 class TestDataService(unittest.TestCase):
@@ -11,26 +8,22 @@ class TestDataService(unittest.TestCase):
     def test_forward_data_ok(self,empty_list):
         data = (1,1,1)
         data_service = ReplicatorReceiver.DataService()
-        data_service.exposed_forward_data(data)
+        data_service.exposed_temporary_store_data(data)
         empty_list.append.assert_called_once()
 
-    @patch("ReplicatorReceiver.data_list")
-    @patch("time.time")
-    def test_try_send_data_not_called(self,empty_data_list,current_time):
-        to_send=[]
-        sent_items=[]
-        empty_data_list=[]
-        data = ReplicatorReceiver.Data((1,1,1),0)
-        empty_data_list.append(data)
-        current_time = 15
+    @patch("ReplicatorReceiver.data_list", [ReplicatorReceiver.Data((1, 1, 1), 0)])
+    @patch("time.time", return_value=15)
+    def test_try_send_data_ok(self, timePatch):
+        to_send = []
+        sent_items = []
 
         replicator_receiver = ReplicatorReceiver.ReplicatorReceiver()
         replicator_conn = MagicMock()
 
-        ReplicatorReceiver.try_send_data(to_send,sent_items,replicator_receiver,replicator_conn)
+        ReplicatorReceiver.try_send_data(to_send, sent_items, replicator_receiver, replicator_conn)
 
-        self.assertEqual(to_send,[])
-        self.assertEqual(sent_items,[])
+        self.assertEqual(to_send, [])
+        self.assertEqual(sent_items, [])
 
 
 if __name__=='__main__':
